90e31a9698c04b2ec25095f7634a43f9b11d8bd7
Michiel Dijkstra <msdijkstra@gmail.com>
2018-04-19 17:14:56 +0200

    init

--- comparemap/settings.py
+++ comparemap/settings.py
@@ -86,10 +86,6 @@ WSGI_APPLICATION = 'comparemap.wsgi.application'
 
 import dj_database_url
 DATABASES = { 'default': dj_database_url.config() }
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
-
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
 
 # Password validation
