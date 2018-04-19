6159ee18645830e833891cbecfb6181e36717ae8
Michiel Dijkstra <msdijkstra@gmail.com>
2018-04-19 16:39:50 +0200

    init

--- comparemap/settings.py
+++ comparemap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*31t-efimik7_zwvz6l!y-xfs*dcfizo3qn0vzq%abb=!cb@nu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost','https://countrycompare.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost','countrycompare.herokuapp.com']
 
 
 # Application definition
