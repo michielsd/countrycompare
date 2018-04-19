6d2ff6bc63c6e662d2cf43869b962aac8b45c518
Michiel Dijkstra <msdijkstra@gmail.com>
2018-04-19 15:36:54 +0200

    init

--- comparemap/settings.py
+++ comparemap/settings.py
@@ -86,8 +86,7 @@ WSGI_APPLICATION = 'comparemap.wsgi.application'
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = 
-'django.contrib.gis.db.backends.postgis'
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
